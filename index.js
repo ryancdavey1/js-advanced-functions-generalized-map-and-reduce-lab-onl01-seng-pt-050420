be82517379f290a81bf7e7f718f7629907eeafda
ryancdavey1 <ryancdavey1@gmail.com>
2020-11-30 11:24:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your functions here
+function map(arr, func) {
+  let r = [];
+  for (let i = 0; i < arr.length; i++ ) {
+    let e = func(arr[i]);
+    r.push(e);
+  }
+  return r
+}
+
+// function reduce(arr, func, startingPoint=0) {
+//   let total = startingPoint;
+//   for (let i = 0; i < arr.length; i++) {
+//     total += func(arr[i], total)
+//   }
+//   return total
+// }
+
+function reduce(src, cb, starting){
+  let r = (!!starting) ? starting : src[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < src.length; i++) {
+    r = cb(src[i], r)
+  }
+
+  return r;
+}
